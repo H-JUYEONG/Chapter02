a372ed9399a5527b99cc0192ed9e0e31d0f102e5
H-JUYEONG <dud9902@gmail.com>
2024-07-09 12:03:17 +0900

    2024.07.09 추상클래스

---
+++ src/com/javaex/ex16/ShapeApp.java
@@ -0,0 +1,32 @@
+package com.javaex.ex16;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+
+		// 도형(사각형, 삼각형, 원)을 Shape 1가지로 관리
+		Shape[] sArray = new Shape[3];
+
+		// 도형 메모리에 올리기
+		Shape r01 = new Rectangle(2, 4, "빨강", "검정");
+		Shape c01 = new Circle(10, "빨강", "검정");
+		Shape t01 = new Triangle(30, 30, "빨강", "검정");
+
+		sArray[0] = r01;
+		sArray[1] = c01;
+		sArray[2] = t01;
+
+		for (int i = 0; i < sArray.length; i++) {
+			sArray[i].draw();
+		}
+
+//		Shape s00 = new Shape("빨강", "검정");
+//		s00.draw();
+ 
+		for (int i = 0; i < sArray.length; i++) {
+			System.out.println(sArray[i].area());
+		}
+
+	}
+
+}
